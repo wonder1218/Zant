8c50cba6d51352bfb1026d63dd5e610815cf6486
wonder1218 <zhengwen_man@163.com>
2023-11-09 11:33:23 +0800

    fixed: 测试ui

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <zw-button type="success">成功按钮</zw-button>
+    <zw-checkbox v-model="checked">复选框</zw-checkbox>
+    <zw-input v-model="inputValue" placeholder="请输入内容"></zw-input>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data(){
+    return {
+      checked: false,
+      inputValue: ''
+    }
+  },
   components: {
     
   }
--- test/src/main.js
+++ test/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import  { ZwButton }  from 'zwantd'
-import 'zwantd/lib/index.css';
+import  { ZwButton,
+  ZwCheckBox,
+  ZwInput, }  from 'zwbasic'
+import 'zwbasic/lib/index.css';
 
 Vue.config.productionTip = false
 Vue.use(ZwButton);
+Vue.use(ZwCheckBox);
+Vue.use(ZwInput);
 
 new Vue({
   render: h => h(App),
